cb7db9dc3bc3542023012ab6eaf14f640fd0c46e
sperkins <sperkins@gmail.com>
2020-09-13 19:10:34 -0700

    adding build directory

---
+++ precache-manifest.3a19c9665ac8d635aaa24838728e9482.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e454af982a936fcdd9b5c7d4b6f1fd",
+    "url": "/keymtg/index.html"
+  },
+  {
+    "revision": "6810cdde87d2b56d9d72",
+    "url": "/keymtg/static/css/main.5b30e378.chunk.css"
+  },
+  {
+    "revision": "67ecc2d71dfaac0949c6",
+    "url": "/keymtg/static/js/2.f48c97e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/keymtg/static/js/2.f48c97e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6810cdde87d2b56d9d72",
+    "url": "/keymtg/static/js/main.33490c4c.chunk.js"
+  },
+  {
+    "revision": "f872173a6697edc10d07",
+    "url": "/keymtg/static/js/runtime-main.c615591f.js"
+  },
+  {
+    "revision": "08e991203f992f39eaca993088859630",
+    "url": "/keymtg/static/media/teferiLUL2.08e99120.png"
+  }
+]);
---
+++ precache-manifest.dcd69aa02d06840aeb005708f1a78805.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a5745d4563c37aa1d6fd6843033a52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59ee76454711b1dfbc8d",
+    "url": "/static/css/main.546f271c.chunk.css"
+  },
+  {
+    "revision": "67ecc2d71dfaac0949c6",
+    "url": "/static/js/2.f48c97e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f48c97e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ee76454711b1dfbc8d",
+    "url": "/static/js/main.0c0fbf21.chunk.js"
+  },
+  {
+    "revision": "05c4a7684145a39aff38",
+    "url": "/static/js/runtime-main.6139a65b.js"
+  }
+]);
